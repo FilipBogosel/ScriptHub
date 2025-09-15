f24fb4eacdebeb7e053a53d6469aba7d86ffb2e5
FilipBogosel <101122946+FilipBogosel@users.noreply.github.com>
2025-09-15 14:30:58 +0300

    refactor(api): remove debug interceptors for request and response logging

--- src/services/api.js
+++ src/services/api.js
@@ -9,18 +9,5 @@ const axiosInstance = axios.create({
     }
 });
 
-// Add a specific debug interceptor for cookies
-axiosInstance.interceptors.request.use(request => {
-    console.log('Request URL:', request.url);
-    console.log('Request with credentials:', request.withCredentials);
-    console.log('Request cookies being sent:', document.cookie);
-    return request;
-});
-
-// Add response interceptor to see what cookies are being set
-axiosInstance.interceptors.response.use(response => {
-    console.log('Response cookies:', response.headers['set-cookie']);
-    return response;
-});
 
 export default axiosInstance;
